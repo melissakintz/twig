b744e094734a0592f75a2c2eda8bd404a2bb1831
Mélissa Kintz <kntzmelissa@gmail.com>
2021-04-08 09:45:58 +0200

    QUETE

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+class HomeController extends AbstractController
+{
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
+
+    public function showProducts()
+    {
+        $products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+        return $this->twig->render('Home/products.html.twig', ['products' => $products]);
+    }
+}
